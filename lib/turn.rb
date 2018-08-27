3f51ed1a277e62f771b52083c09e9a09a5925bb3
Alexei Lourie <alexeilourie@gmail.com>
2018-08-27 19:14:32 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -19,8 +19,12 @@ def display_board(board)
 end
 
 def valid_move?(board, index)
-  if index >= 0 && index < 9 && !(board[index] == "X" || board[index] == "O")
-    return true
+  if index >= 0 && index < 9 && 
+    if board[index] == "X" || board[index] == "O"
+      return false
+    else
+      return true
+    end
   else
     return false
   end
